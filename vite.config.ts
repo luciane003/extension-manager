5eb2e80cd9ca0df9fd789679a8dfeb916a663919
luciane003 <lucianekellenpsarges@gmail.com>
2026-06-15 15:33:41 -0300

    Ajuste de base para deploy no Vercel

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: process.env.VERCEL ? '/' : '/extension-manager/',
+  // base: process.env.VERCEL ? '/' : '/extension-manager/',
+  base : '/',
   plugins: [react()],
 })
